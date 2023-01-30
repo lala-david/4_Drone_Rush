dd92e7f28fde90a42990e5d7c84114458a9e8ef9
SeongJun <37481441+lala-david@users.noreply.github.com>
2023-01-30 16:58:14 +0900

    Add files via upload

---
+++ src/Object_Recognition.py.py
@@ -0,0 +1,35 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+
+human_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+fps = cap.get(cv2.CAP_PROP_FPS)
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+delay = round(1000/fps)
+w = round(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+#out = cv2.VideoWriter('output.avi', fourcc, fps, (w, h))
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    humans = human_cascade.detectMultiScale(gray, 1.9, 1)
+    
+    # Display the resulting frame
+    for (x,y,w,h) in humans:
+         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+         
+         
+
+    cv2.imshow('frame',frame)
+    #out.write(frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
